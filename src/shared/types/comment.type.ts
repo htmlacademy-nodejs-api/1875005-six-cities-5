2e88a93db9cd0d2a7ba4df0100509c44329d6dc0
«Roman <romanzelenenko@yandex.ru>
2023-11-03 14:22:47 -0400

    feat: add specification yml

--- src/shared/types/comment.type.ts
+++ src/shared/types/comment.type.ts
@@ -1,7 +1,7 @@
 import { User } from './user.type.js';
 
 export type Comment = {
-  omment: string; // 5-1024 symbols
+  comment: string; // 5-1024 symbols
   date: string; // Obligatory, not used for add comment
   rating: number; // Obligatory, from 1 to 5
   user: User; // Obligatory
